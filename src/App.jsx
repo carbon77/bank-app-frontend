1906477664a563574f8a010ef9890867d6b84c1e
Igor <42415653+carbon77@users.noreply.github.com>
2024-03-27 12:36:21 +0300

    Created store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import {Router} from "./Router";
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./theme";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 function App() {
     return (
         <div className="App">
-            <ThemeProvider theme={theme}>
-                <Router/>
-            </ThemeProvider>
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <Router/>
+                </ThemeProvider>
+            </Provider>
         </div>
     );
 }
